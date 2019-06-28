cc27fb8aa6033398181b2170adca11f1ad191ca9
Bernie van Veen <b.vanveen@vwt.digital>
2019-06-28 23:15:54 +0200

    Backup all git repos to backup bucket. DAT-277

--- backup/repositories/backup_github_repos.sh
+++ backup/repositories/backup_github_repos.sh
@@ -2,10 +2,11 @@
 
 data_catalog_file=${1}
 PROJECT_ID=${2}
-keyring_region=${3}
-keyring=${4}
-key=${5}
-b64_encrypted_github_token=${6}
+dest_bucket=${3}
+keyring_region=${4}
+keyring=${5}
+key=${6}
+b64_encrypted_github_token=${7}
 
 if [ -z "${PROJECT_ID}" ]
 then
@@ -18,7 +19,6 @@ basedir=$(dirname $0)
 if [ -n "${key}" ]
 then
     echo "decrypting github access token using key ${key}@${keyring}"
-    echo "encrypted token: ${b64_encrypted_github_token}"
     github_token=$(echo ${b64_encrypted_github_token} | base64 -d - | gcloud kms decrypt \
         --key=${key} \
         --keyring=${keyring} \
@@ -26,16 +26,30 @@ then
         --ciphertext-file=- \
         --plaintext-file=- \
         --project=${PROJECT_ID})
+
+    if [ $? -ne 0 ]
+    then
+        echo "Error decoding github access token"
+        exit 1
+    fi
 fi
 
 git clone --branch=0.24.0 https://github.com/josegonzalez/python-github-backup.git
 export PYTHONPATH=python-github-backup
 mkdir output
 
-for repo in $(python3 ${basedir}/list_github_repos.py ${data_catalog_file} | head -n1)
+for repo in $(python3 ${basedir}/list_github_repos.py ${data_catalog_file})
 do
-   echo "Backup ${repo} from ${PROJECT_ID}"
+   echo "Create backup of ${repo} from ${PROJECT_ID}"
    organization=$(echo $repo | cut -d/ -f1)
-   reponame=$(echo $repo | cut -d/ -f2)
-   python-github-backup/bin/github-backup --token=${github_token} --all --organization --repository=${reponame%.*} --output-directory=output ${organization}
+   reponame=$(echo $repo | cut -d/ -f2 | cut -d. -f1)
+   python-github-backup/bin/github-backup --token=${github_token} --all --organization --repository=${reponame} --output-directory=output ${organization}
+
+   destpath="gs://${dest_bucket}/github/$(date +%Y/%m/%d)/${reponame}.tgz"
+   echo "Copy backup of ${repo} to ${destpath}"
+   cd output/repositories
+   tar -zcvf ${reponame}.tgz ${reponame}
+   gsutil cp ${reponame}.tgz ${destpath}
+   rm -rf ${reponame} ${reponame}.tgz
+   cd ../..
 done
