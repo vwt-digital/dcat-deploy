9e9b907ae5be04b868fd876ed0fbc7cbfc6d7c62
Eric van der Velde <e.vandervelde@vwt.digital>
2019-09-11 13:37:44 +0200

    Try some other pip. DAT-727

--- deploy_dcat_gcp.sh
+++ deploy_dcat_gcp.sh
@@ -30,7 +30,7 @@ then
       --key=github-access-token
 
     ${dcat_deploy_dir}/catalog/repos/create_github_repos.sh ${data_catalog_path} ${dcat_deploy_dir}/catalog/repos/github_access_token.key
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
     python3 ${dcat_deploy_dir}/catalog/repos/setup_github_team_repos.py ${data_catalog_path} ${dcat_deploy_dir}/catalog/repos/github_access_token.key
 fi
 
