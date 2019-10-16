c6fc1b462741d32960b76997d264998aa195d9d9
Jules van Enckevort <j.vanenckevort@vwt.digital>
2019-10-16 15:04:38 +0200

    Add dest bucket

--- backup/cloudsql/list_cloudsql_databases.py
+++ backup/cloudsql/list_cloudsql_databases.py
@@ -9,4 +9,4 @@ for entry in catalog['dataset']:
     for dist in entry.get('distribution', []):
         if 'title' in dist and dist.get('format', 'n/a') == 'mysql-db':
             # Delimiter between instance and database name for bash handling
-            print(dist['deploymentProperties']['instance'] + | + dist['title'])
+            print(dist['deploymentProperties']['instance'] + '|' + dist['title'])
