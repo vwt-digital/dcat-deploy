44f8e806911e2d801d84b9a1aa56a4e747b46500
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-04-03 08:28:03 +0200

    DAT-2458. Change substitution data catalog

--- backup/substitute_data_catalog.py
+++ backup/substitute_data_catalog.py
@@ -77,14 +77,14 @@ permissions = [
 for outer, dataset in enumerate(catalog.get('dataset')):
     dataset.pop('odrlPolicy', None)
     if dataset.get('identifier') == old_project_id + '-dcat-deployed-stg':
-        catalog.get('dataset').pop(outer)
+        catalog['dataset'].pop(outer)
     for inner, distribution in enumerate(dataset.get('distribution')):
         title = distribution.get('title')
         distribution = dict_replace_value(distribution, old_project_id, new_project_id)
         distribution['backupSource'] = title
-        dataset.get('distribution')[inner] = distribution
+        dataset['distribution'][inner] = distribution
         if distribution.get('title') == '{}-firestore-ephemeral-backup-stg'.format(new_project_id):
-            dataset.get('distribution').pop(inner)
+            dataset['distribution'].pop(inner)
         if distribution.get('format') == 'cloudsql-instance':
             permissions.append({
                 "target": "{}-tmp-backup-stg".format(new_project_id),
