74f2e42cd227f38cc1e08864313e5256e07bb392
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-04-16 20:11:01 +0200

    DAT-2654. Add legacy access policy deployment for bigquery

--- catalog/scripts/deploy_data_catalog.py
+++ catalog/scripts/deploy_data_catalog.py
@@ -223,7 +223,7 @@ def gather_permissions(access_level, resource_title, resource_format, project_id
 def get_bigquery_access_policy(resource, odrlPolicy):
     access_policy = []
     for permission in odrlPolicy.get('permission', []):
-        if permission['title'] == resource['title']:
+        if permission['target'] == resource['title']:
             if 'serviceAccount' in permission['assignee'] or 'user' in permission['assignee']:
                 access_policy.append({
                     'role': '',
