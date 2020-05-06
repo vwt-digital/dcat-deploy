f3a93148705a06f082bf388c745fe79e427947bc
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-05-06 19:46:09 +0200

    DAT-2753. Backup/restore bigquery datasets

--- backup/substitute_data_catalog.py
+++ backup/substitute_data_catalog.py
@@ -74,12 +74,8 @@ permissions = [
     }
 ]
 
-for outer, dataset in enumerate(catalog.get('dataset')):
+for outer, dataset in enumerate(catalog.get('dataset', [])):
     dataset.pop('odrlPolicy', None)
-    if dataset.get('identifier') == old_project_id + '-dcat-deployed':
-        del catalog['dataset'][outer]
-    if dataset.get('identifier') == old_project_id + '-firestore-ephemeral-backup':
-        del catalog['dataset'][outer]
     for inner, distribution in enumerate(dataset.get('distribution')):
         title = distribution.get('title')
         distribution = dict_replace_value(distribution, old_project_id, new_project_id)
@@ -91,6 +87,9 @@ for outer, dataset in enumerate(catalog.get('dataset')):
                 "action": "read",
                 "assignee": "serviceAccount:$(ref.{}.serviceAccountEmailAddress)".format(distribution.get('title'))
             })
+
+exclude = [old_project_id + '-firestore-ephemeral-backup', old_project_id + '-dcat-deployed']
+catalog['dataset'] = [ds for ds in catalog.get('dataset', []) if not ds['identifier'] in exclude]
 
 catalog.get('dataset', []).append({
   "identifier": "{}-tmp-backup".format(new_project_id),
