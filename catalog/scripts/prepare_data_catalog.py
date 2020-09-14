9987ad31a9e2c63ae4f33ed80fefb20460885065
SpringPie <bente.vandepol@gmail.com>
2020-09-14 12:08:22 +0200

    Delegated SA needs access to buckets DAT-4108

--- catalog/scripts/prepare_data_catalog.py
+++ catalog/scripts/prepare_data_catalog.py
@@ -152,7 +152,7 @@ for i, dataset in enumerate(catalog.get('dataset', [])):
                         delegated_sa_permission = {
                           "target": "{}-history-stg".format(distribution.get('title')),
                           "assignee": "serviceAccount:{}".format(delegated_sa),
-                          "action": "modify"
+                          "action": "roles/storage.objectViewer"
                         }
                         dataset['odrlPolicy']['permission'].append(delegated_sa_permission)
 
