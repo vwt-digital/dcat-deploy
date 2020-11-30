6952f7f5a0177583d93cc417e3f2b4da992c4839
Eric van der Velde <e.vandervelde@vwt.digital>
2020-11-30 12:09:21 +0100

    pubsub backup tuning. DAT-4885

--- history/create_history_function.sh
+++ history/create_history_function.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# shellcheck disable=SC2140
+
 PROJECT_ID=${1}
 BRANCH_NAME=${2}
 SERVICE_ACCOUNT=${3}
@@ -7,17 +9,17 @@ SERVICE_ACCOUNT=${3}
 echo "Creating topic history function(s)..."
 
 echo " + Cloning pubsub-backup repo..."
-git clone --branch=${BRANCH_NAME} https://github.com/vwt-digital/pubsub-backup.git
+git clone --branch="${BRANCH_NAME}" https://github.com/vwt-digital/pubsub-backup.git
 (cd pubsub-backup/functions/pubsub-backup && gcloud functions deploy "${PROJECT_ID}-history-func" \
   --entry-point=handler \
   --runtime=python37 \
   --trigger-http \
-  --project=${PROJECT_ID} \
+  --project="${PROJECT_ID}" \
   --region=europe-west1 \
   --memory=1024MB \
   --timeout=540s \
   --service-account="${SERVICE_ACCOUNT}" \
-  --set-env-vars=PROJECT_ID=${PROJECT_ID},MAX_RETRIES="3",MAX_MESSAGES="1000",TOTAL_MESSAGES="250000")
+  --set-env-vars=PROJECT_ID="${PROJECT_ID}",MAX_RETRIES="1",MAX_SMALL_BATCHES="5",MAX_MESSAGES="1000",TOTAL_MESSAGES="250000")
 
 echo " + Setting permissions for ${PROJECT_ID}-history-func..."
 
@@ -27,5 +29,5 @@ EOF
 
 gcloud beta functions set-iam-policy "${PROJECT_ID}-history-func" \
   --region=europe-west1 \
-  --project=${PROJECT_ID} \
+  --project="${PROJECT_ID}" \
   history_func_permissions.json
